8f57d0b95d722300afd56ac2e3cdb978ae46f29d
OmorfiKQ <okqcares@gmail.com>
2025-10-18 17:17:30 -0400

    added JS timing events

--- src/script.js
+++ src/script.js
@@ -1,3 +1,32 @@
+
+//Lesson4: JS Timing events
+
+function showTime() {
+    
+let currentDateELement = document.querySelector("#date");
+
+currentDateELement.innerHTML = new Date();
+    
+} 
+
+setTimeout(showTime, 5000);
+setInterval(showTime, 1000);
+
+
+function displayMilliSeconds() {
+  
+    let dateELement = new Date();
+
+    let millisecondsELement = document.querySelector("#milliseconds");
+
+    millisecondsELement.innerHTML = dateELement.getMilliseconds();
+} 
+
+setInterval(displayMilliSeconds, 100);
+
+
+
+//Lesson3: HTML Select Element
 function showSelected(event) {
     if (event.target.value.length > 0){
         if (event.target.value === "blue"){
